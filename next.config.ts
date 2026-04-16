0302004b8a76b82a777d1d749804bffcb6ffd72d
kravchenski <max5045955@gmail.com>
2026-04-16 18:21:21 +0200

    feat: configure image optimization settings in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [360, 414, 640, 768, 1024, 1280, 1536],
+    imageSizes: [96, 110, 128, 256, 384],
+    qualities: [60, 70, 72, 75],
+  },
 };
 
 export default nextConfig;
